4ee1ea036341ef04fd1f2885de6c56b83902271a
Facundo Vallejos <Facund097dv@gmail.com>
2023-12-17 18:25:11 -0300

    tarea 3 calcular tiempo videos terminada

--- task-3/task3.js
+++ task-3/task3.js
@@ -21,13 +21,13 @@ function getNumberOfVideos() {
 
 function createElements(quantityElements) {
     let $divFather = document.createElement("div");
-    $divFather.setAttribute("class","div-father");
+    $divFather.setAttribute("class", "div-father");
     const textPlaceholder = ["Horas", "Minutos", "Segundos"];
     const inputClassName = ["hours", "minutes", "seconds"];
 
     for (let j = 0; j < quantityElements; j++) {
         $label = document.createElement("label");
-        $label.innerText = `Video #${j+1}`;
+        $label.innerText = `Video #${j + 1}`;
         $divFather.appendChild($label);
 
         for (let i = 0; i < inputClassName.length; i++) {
@@ -41,7 +41,6 @@ function createElements(quantityElements) {
         $divFather.appendChild(document.createElement("br"));
     }
 
-
     console.log($divFather);
 
     return $divFather;
@@ -54,14 +53,74 @@ function insertElements($elements) {
 }
 
 function clearInputs() {
-
     document.querySelector(".div-father").remove();
-
 }
 
 function verifiedInputs() {
     let $divFather = document.querySelectorAll(".div-father");
-    if($divFather.length !== 0){
+    if ($divFather.length !== 0) {
         clearInputs();
     }
+}
+
+//botón calcular tiempo
+let $calculateButton = document.querySelector(".calculate-button");
+
+$calculateButton.onclick = function (event) {
+    let totalHours = getTotalHours();
+    let totalMinutes = getTotalMinutes();
+    let totalSeconds = getTotalSeconds();
+    let totalTime = calculateTotalTime(totalHours, totalMinutes, totalSeconds);
+    showTotalTime(totalTime);
+    event.preventDefault();
+};
+
+function getTotalHours() {
+    let $hours = document.querySelectorAll(".hours");
+    let totalHours = 0;
+
+    for (let i = 0; i < $hours.length; i++) {
+        totalHours += Number($hours[i].value);
+    }
+
+    console.log(totalHours);
+    return totalHours;
+}
+
+function getTotalMinutes() {
+    let $minutes = document.querySelectorAll(".minutes");
+    let totalMinutes = 0;
+
+    for (let i = 0; i < $minutes.length; i++) {
+        totalMinutes += Number($minutes[i].value);
+    }
+
+    console.log(totalMinutes);
+    return totalMinutes;
+}
+
+function getTotalSeconds() {
+    let $seconds = document.querySelectorAll(".seconds");
+    let totalSeconds = 0;
+
+    for (let i = 0; i < $seconds.length; i++) {
+        totalSeconds += Number($seconds[i].value);
+    }
+    console.log(totalSeconds);
+    return totalSeconds;
+}
+
+function calculateTotalTime(hours, minutes, seconds) {
+    minutes += seconds < 60 ? 0 : Math.floor(seconds / 60);
+    seconds = seconds % 60;
+    hours += minutes < 60 ? 0 : Math.floor(minutes / 60);
+    minutes = minutes % 60;
+
+    return [hours, minutes, seconds];
+}
+
+function showTotalTime(totalTime) {
+    let $textTotalTime = document.querySelector(".total-time");
+
+    $textTotalTime.innerText = `Horas: ${totalTime[0]} Minutos: ${totalTime[1]} Segundos: ${totalTime[2]}`;
 }
