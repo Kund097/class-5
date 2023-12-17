3af5fc45483e619736e90a3170f52e24a0014e2a
Facundo Vallejos <Facund097dv@gmail.com>
2023-12-16 23:36:22 -0300

    agrega funcionalidad para crear cantidad de inputs ingresados

--- task-3/task3.js
+++ task-3/task3.js
@@ -6,9 +6,62 @@
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
 
-let $calculateButton = document.querySelector(".calculate-button");
-$calculateButton.onclick = function (event) {
-    console.log("click");
+let $enterButton = document.querySelector(".enter-button");
+$enterButton.onclick = function (event) {
+    verifiedInputs();
+    $elements = createElements(getNumberOfVideos());
+    insertElements($elements);
 
     event.preventDefault();
 };
+
+function getNumberOfVideos() {
+    return Number(document.querySelector(".number-of-videos").value);
+}
+
+function createElements(quantityElements) {
+    let $divFather = document.createElement("div");
+    $divFather.setAttribute("class","div-father");
+    const textPlaceholder = ["Horas", "Minutos", "Segundos"];
+    const inputClassName = ["hours", "minutes", "seconds"];
+
+    for (let j = 0; j < quantityElements; j++) {
+        $label = document.createElement("label");
+        $label.innerText = `Video #${j+1}`;
+        $divFather.appendChild($label);
+
+        for (let i = 0; i < inputClassName.length; i++) {
+            $input = document.createElement("input");
+            $input.setAttribute("placeholder", `${textPlaceholder[i]}`);
+            $input.setAttribute("type", "number");
+            $input.setAttribute("class", `${inputClassName[i]}`);
+            $divFather.appendChild($input);
+        }
+
+        $divFather.appendChild(document.createElement("br"));
+    }
+
+
+    console.log($divFather);
+
+    return $divFather;
+}
+
+function insertElements($elements) {
+    let $divInputTime = document.querySelector(".inputs-time");
+
+    $divInputTime.appendChild($elements);
+}
+
+function clearInputs() {
+
+    document.querySelector(".div-father").remove();
+
+}
+
+function verifiedInputs() {
+    let $divFather = document.querySelectorAll(".div-father");
+    if($divFather.length !== 0){
+        clearInputs();
+    }
+}
